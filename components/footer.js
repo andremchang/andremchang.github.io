098d13bee6521b2281247e1611bd76781194313e
ANDRE M. CHANG <144941527+andremchang@users.noreply.github.com>
2024-03-03 21:30:13 -0300

    Add files via upload

---
+++ components/footer.js
@@ -0,0 +1,48 @@
+﻿class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <style>
+        footer {
+          height: auto;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background-color:  #f8f8f8;
+        }
+        
+        ul li {
+          list-style: none;
+          display: flex;
+          justify-content: center;
+        }
+             
+        .social-row {
+          font-size: auto;
+        }
+        
+        .social-row li a {
+          margin: 0 14px;
+        }
+      </style>
+      
+      <footer>
+        <ul>
+          <li><small>Copyright © 2024 ANDRE M. CHANG | AMC VISUAL. All rights reserved.</small></li>
+          <li><small>Last updated: 3 Mar 2024</small></li>
+        </ul>
+        <ul class="social-row">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="https://mastodon.social/@andremchang">Mastodon</a></li>
+        </ul>
+        <br>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', Footer);
---
+++ components/header.js
@@ -0,0 +1,49 @@
+﻿class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+  
+  connectedCallback() {
+    this.innerHTML = `
+      <style>
+        header {
+          height: auto;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background-color:  #f8f8f8;
+        }
+        
+        ul li {
+          list-style: none;
+          display: flex;
+          justify-content: center;
+        }
+                    
+        .social-row {
+          font-size: auto;
+        }
+        
+        .social-row li a {
+          margin: 0 14px;
+        }   
+      </style>
+      
+      <header>
+        <ul>
+          <li>Hello / Hola / Olá / <ruby>你好<rt>ㄋㄧˇㄏㄠˇ</rt></ruby> / ハロー / Maitei</li>
+          <li><b>ANDRE M. CHANG</b></li>
+        </ul>
+        <ul>
+          <li><img alt="ANDRE M. CHANG" width="80px" src="ivj2_400.png" style="height: 84px"></li>        
+        </ul>
+        <ul class="social-row">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </header>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
