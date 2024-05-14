a22026db2d1e3619e7989eb6631b2fccfa026c7f
ANDRE M. CHANG <144941527+andremchang@users.noreply.github.com>
2024-05-13 22:26:09 -0400

    Add files via upload

--- components/header.js
+++ components/header.js
@@ -33,9 +33,6 @@
           <li><b>ANDRE M. CHANG</b></li>
 		  <li><a href="index.html">Home</a>&nbsp;|&nbsp;<a href="https://andremchang.super.site/showcase">Showcase</a>&nbsp;|&nbsp;<a href="online-presence.html">Online presence</a>&nbsp;|&nbsp;<a href="blog-post.html">Blog</a></li>
 		</ul>
-        <ul class="header-row">
-
-		</ul>
 		  <ul class="header-row">
 		  <li><button id='dark-mode-toggle'><style="font-size:16px">&#127768 &#127774</button></li>
         </ul>
