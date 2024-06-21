2815aa34d9bd8bee8a5bd97046a6aaaf175e7069
ANDRE M. CHANG <144941527+andremchang@users.noreply.github.com>
2024-06-20 21:05:26 -0400

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -39,7 +39,7 @@
 			</nav>
 			<li><small>Website handcrafted by human not by AI. Made with love.</small></li>
 			<li><small>Copyright © 2024 ANDRE M. CHANG | AMC VISUAL. All rights reserved.</small></li>
-			<li><small>Updated on June 17, 2024</small></li>
+			<li><small>Updated on June 20, 2024</small></li>
 		</ul>
 	</footer>
 
