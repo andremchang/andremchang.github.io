3bc7bb9843312c90cd74836b41b513a851178e8d
ANDRE M. CHANG <144941527+andremchang@users.noreply.github.com>
2024-09-15 12:31:07 -0400

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -31,12 +31,12 @@
 	<footer>
 		<ul class="footer-row">
 			<nav>
-			<a href="https://andremchang.github.io/index.html">Home</a> | <a href="https://andremchang.github.io/blog.html">Blog</a> | <a href="https://andremchang.github.io/online-presence.html">Links</a><br>
+			<a href="https://andremchang.github.io/index.html">Home</a> | <a href="https://andremchang.github.io/blog.html">Blog</a> | <a href="https://andremchang.github.io/links.html">Links</a><br>
 			<a href="https://andremchang.super.site/published-work">Publications</a> | <a href="https://andremchang.super.site/showcase">Showcase</a>
 			</nav>
 			<li><small>Website handcrafted by human not by AI. Made with love.</small></li>
 			<li><small>Copyright © 2023-2024 ANDRE M. CHANG | AMC VISUAL. All rights reserved.</small></li>
-			<li><small>Updated on September 14, 2024</small></li>
+			<li><small>Updated on September 15, 2024</small></li>
 		</ul>
 	</footer>
 
