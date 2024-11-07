b8fcfca20ad2c71c3c024b4457f0457a53d35420
ANDRE M. CHANG <144941527+andremchang@users.noreply.github.com>
2024-11-06 22:57:44 -0300

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -34,10 +34,10 @@
 			<a href="https://andremchang.github.io/index.html">Home</a> | <a href="https://andremchang.github.io/contact.html">Contact</a> | <a href="https://andremchang.github.io/blog.html">Blog</a> | <a href="https://andremchang.github.io/links.html">Links</a><br>
 			<a href="https://andremchang.super.site/published-work">Publications</a> | <a href="https://andremchang.super.site/showcase">Showcase</a>
 			</nav>
-			<li><small>Website handcrafted by human not by AI. Made with love.</small></li>
+			<li><small>This site is made with ❤︎ Handcrafted by human not by AI.</small></li>
 			<li><small>Copyright © 2023-2024 ANDRE M. CHANG | AMC VISUAL. All rights reserved.
 			<li><small>Fingerprint: B0C3 C562 C765 ECE8 C110 7897 FDE0 F655 E348 A81E</small> <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/B0C3C562C765ECE8C1107897FDE0F655E348A81E"><small>[Public key]</small></a></li>
-			<li><small>Updated on November 4, 2024</small></li>
+			<li><small>Updated on November 6, 2024</small></li>
 		</ul>
 	</footer>
 
