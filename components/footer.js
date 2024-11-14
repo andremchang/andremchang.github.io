1fc418c957562001f672028118ceda3d0e0cd8a5
ANDRE M. CHANG <144941527+andremchang@users.noreply.github.com>
2024-11-13 22:59:32 -0300

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -1,44 +1,45 @@
 ﻿﻿class Footer extends HTMLElement {
 	constructor() {
-		super();}
+		super();
+	  }
+	  
+	  connectedCallback() {
+	  this.innerHTML = `
+		<style>
+			footer {
+				height: auto;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				background-color: auto;}
 
-	connectedCallback() {
-		this.innerHTML = `
-	<style>
-		footer {
-			height: auto;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			background-color: auto;}
+			ul li {
+				<!-- font-family: Inconsolata; -->
+				font-size: auto;
+				line-height: auto;
+				list-style: none;
+				display: flex;
+				justify-content: center;
+				margin: 0px;
+				padding: 0px;}
 
-		ul li {
-			<!-- font-family: Inconsolata; -->
-			font-size: auto;
-			line-height: auto;
-			list-style: none;
-			display: flex;
-			justify-content: center;
-			margin: 0px;
-			padding: 0px;}
+			.footer-row {
+				font-size: auto;
+				margin: 7px;
+				padding: 0px;}
+		</style>
 
-		.footer-row {
-			font-size: auto;
-			margin: 7px;
-			padding: 0px;}
-	</style>
-
-	<footer>
-		<ul class="footer-row">
-			<li><a href="#top">⬆</a></li> 
-			<li><small>This site is made with ❤︎ Handcrafted by human not by AI.</small></li>
-			<li><small>Copyright © 2023-2024 ANDRE M. CHANG | AMC VISUAL. All rights reserved.
-			<li><small>Fingerprint: B0C3 C562 C765 ECE8 C110 7897 FDE0 F655 E348 A81E</small> <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/B0C3C562C765ECE8C1107897FDE0F655E348A81E"><small>[Public key]</small></a></li>
-			<li><small>Updated on November 7, 2024</small></li>
-		</ul>
-	</footer>
-
-	<script src="https://andremchang.github.io/components/script.js"></script>`;}
+		<footer>
+			<ul class="footer-row">
+				<li><a href="#top">⬆</a></li> 
+				<li><small>This site is made with ❤︎ Crafted by human not by AI.</small></li>
+				<li><small>Copyright © 2024 ANDRE M. CHANG | AMC VISUAL. All rights reserved.
+				<li><small>Fingerprint: B0C3 C562 C765 ECE8 C110 7897 FDE0 F655 E348 A81E</small> <a href="https://keys.openpgp.org/vks/v1/by-fingerprint/B0C3C562C765ECE8C1107897FDE0F655E348A81E"><small>[Public key]</small></a></li>
+				<li><small>Updated on November 13, 2024</small></li>
+			</ul>
+		</footer>
+`;
+}
 }
 
 customElements.define('footer-component', Footer);
--- components/header.js
+++ components/header.js
@@ -1,45 +1,46 @@
 ﻿﻿class Header extends HTMLElement {
 	constructor() {
-		super();}
+	  super();
+	}
+    
+    connectedCallback() {
+    this.innerHTML = `
+		<style>
+			header {
+				height: auto;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				background-color: auto;}
 
-	connectedCallback() {
-		this.innerHTML = `
-	<style>
-		header {
-			height: auto;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			background-color: auto;}
+			ul li {
+				<!-- font-family: Inconsolata; -->
+				font-size: auto;
+				line-height: auto;
+				list-style: none;
+				display: flex;
+				justify-content: center;
+				margin: 0px;
+				padding: 0px;}
 
-		ul li {
-			<!-- font-family: Inconsolata; -->
-			font-size: auto;
-			line-height: auto;
-			list-style: none;
-			display: flex;
-			justify-content: center;
-			margin: 0px;
-			padding: 0px;}
+			.header-row {
+				font-size: auto;
+				margin: 7px;
+				padding: 0px;}
+		</style>
 
-		.header-row {
-			font-size: auto;
-			margin: 7px;
-			padding: 0px;}
-	</style>
-
-	<header>
-		<ul class="header-row">
-				<li><img class="u-photo" src="https://andremchang.github.io/assets/headshot_ae.png" width="90px" height="90px" alt="headshot"/></li>
-			<span class="bblue"><small><a href="https://andremchang.github.io/about-me.html">en</a></small></span> <span class="byellow"><small><a href="https://andremchang.github.io/sobre-mi.html">es</a></small></span> <span class="bgreen"><small><a href="https://andremchang.github.io/sobre-min.html">pt</a></small></span> <span class="bred"><small><a href="https://andremchang.github.io/about-zh.html">zh</a></small></span>
-			<nav>
-				<a href="https://andremchang.github.io/index.html">Home</a> | <a href="https://andremchang.github.io/contact.html">Contact</a> | <a href="https://andremchang.github.io/blog.html">Blog</a> | <a href="https://andremchang.github.io/links.html">Links</a><br>
-				<a href="https://andremchang.super.site/published-work">Publications</a> | <a href="https://andremchang.super.site/showcase">Showcase</a> 
-			</nav>
-		</ul>
-	</header>
-
-	<script src="https://andremchang.github.io/components/script.js"></script>`;}
+		<header>
+			<ul class="header-row">
+					<span><a href="https://andremchang.github.io/index.html"><img class="u-photo" src="https://andremchang.github.io/assets/headshot_ae.png" width="70px" height="70px" alt="headshot"/></a></span><br>
+					<span class="bblue"><small><a href="https://andremchang.github.io/about-me.html">en</a></small></span> <span class="byellow"><small><a href="https://andremchang.github.io/sobre-mi.html">es</a></small></span> <span class="bgreen"><small><a href="https://andremchang.github.io/sobre-min.html">pt</a></small></span> <span class="bred"><small><a href="https://andremchang.github.io/about-zh.html">zh</a></small></span><br><br>
+				<nav>
+					<a href="https://andremchang.github.io/index.html">Home</a> | <div class="dropdown"><button class="dropbtn">Projects</button><div class="dropdown-content">
+					<a href="https://andremchang.github.io/project/japanese_aesthetics.html">Japanese Aesthetics</a></div></div> | <a href="https://andremchang.github.io/blog.html">Blog</a> | <a href="https://andremchang.github.io/links.html">Links</a> | <a href="https://andremchang.github.io/contact.html">Contact</a> | <a href="https://andremchang.super.site/published-work">Publications</a> | <a href="https://andremchang.super.site/showcase">Showcase</a> 
+				</nav>
+			</ul>
+		</header>
+`;
+}
 }
 
 customElements.define('header-component', Header);
