ed5df371e5ecd9e99a5470bb74c8af79cd10cb5e
ANDRE M. CHANG <144941527+andremchang@users.noreply.github.com>
2024-12-16 00:41:19 -0300

    Add files via upload

--- components/header.js
+++ components/header.js
@@ -34,14 +34,14 @@
 					<span><a href="https://andremchang.github.io"><img class="u-photo" src="https://andremchang.github.io/assets/headshot_ae.png" width="70px" height="70px" alt="headshot"/></a></span>
 					<span class="bblue"><small><a href="https://andremchang.github.io/about-me">en</a></small></span> <span class="byellow"><small><a href="https://andremchang.github.io/sobre-mi">es</a></small></span> <span class="bgreen"><small><a href="https://andremchang.github.io/sobre-min">pt</a></small></span> <span class="bred"><small><a href="https://andremchang.github.io/about-zh">zh</a></small></span><br><br>
 				<nav>
-					<a href="https://andremchang.github.io">Home</a> | 
+					<a href="https://andremchang.github.io">Home</a> | <a href="https://andremchang.github.io/publications">Publications</a> | 
 					<div class="dropdown"><button class="dropbtn">Projects</button>
 						<div class="dropdown-content">
 						<a href="https://andremchang.github.io/japanese_aesthetics">Japanese Aesthetics</a>
 						<a href="https://andremchang.github.io/everyday_life">Snapshot of Everyday Life</a>
 						</div>
 					</div> | 
-					<a href="https://andremchang.github.io/blog">Blog</a> | <a href="https://andremchang.github.io/links">Links</a> | <a href="https://andremchang.github.io/contact">Contact</a> | <a href="https://andremchang.super.site/published-work">Publications</a> | <a href="https://andremchang.super.site/showcase">Showcase</a> 
+					<a href="https://andremchang.github.io/blog">Blog</a> | <a href="https://andremchang.github.io/links">Links</a> | <a href="https://andremchang.github.io/contact">Contact</a>
 				</nav>
 			</ul>
 		</header>
