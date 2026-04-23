a638d0e98cda4b20978a1f12de2d93e4b1436b08
ANDRÉ M. CHANG <144941527+andremchang@users.noreply.github.com>
2026-04-23 11:09:35 +0800

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -35,7 +35,7 @@
 				<li><small>This site is made with &#129505;. Crafted by human not by AI. Text and images by <i>Homo sapiens</i>.</small></li>
 				<li><small>Copyright &#169; 2026 ANDRE M. CHANG | AMC VISUAL. All rights reserved.<br>Prohibited for Generative AI/ML training.
 				<li><small>Fingerprint: B0C3 C562 C765 ECE8 C110 7897 FDE0 F655 E348 A81E</small> <a href="https://keys.openpgp.org/search?q=B0C3+C562+C765+ECE8+C110+7897+FDE0+F655+E348+A81E" target="_blank"><small>[Public key]</small></a></li>
-				<li><small>Updated on April 9, 2026.</small></li>
+				<li><small>Updated on April 23, 2026.</small></li>
 			</ul>
 		</footer>
 `;
