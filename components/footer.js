5d1198cfc117d5619d8eb4df32918d4586a5589c
ANDRÉ M. CHANG <144941527+andremchang@users.noreply.github.com>
2026-08-17 20:48:39 +0800

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -34,11 +34,13 @@
 			
 				<li><small><a href="#top">&uarr;</a></small>
 				</li> 
-				<li><small>This site is made with &#129505;. Crafted by human, not AI. Text and images by <i>Homo sapiens</i>.</small>
+				<li><small>This site is made with &#129505;, crafted by human, not AI. Text and images by <i>Homo sapiens</i>.</small>
 				</li>
 				<li><small>Copyright &#169; 2026 ANDRÉ M. CHANG. All rights reserved.<br>Prohibited for Generative AI/ML training.</small>
 				</li>
-				<li><small>Fingerprint: B0C3 C562 C765 ECE8 C110 7897 FDE0 F655 E348 A81E</small>&nbsp;<a href="https://keys.openpgp.org/search?q=B0C3+C562+C765+ECE8+C110+7897+FDE0+F655+E348+A81E" target="_blank"><small>[Public key]</small></a>
+				<li><small>Fingerprint: B0C3 C562 C765 ECE8 C110 7897 FDE0 F655 E348 A81E</small>
+				</li>
+				<li><small><a href="https://keys.openpgp.org/search?q=B0C3+C562+C765+ECE8+C110+7897+FDE0+F655+E348+A81E" target="_blank"><small>[Public key]</small></a>
 				</li>
 				<li><small>Updated on Aug 17, 2026.</small>
 				</li>
