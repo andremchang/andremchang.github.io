f73540ed3471d77a2cf100eb7d2cf7fa8c8e05e0
ANDRÉ M. CHANG <144941527+andremchang@users.noreply.github.com>
2026-08-25 13:08:58 +0800

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -32,21 +32,46 @@
 		<footer>
 			<ul class="footer-row">
 			
-				<li><small><a href="#top">&uarr;</a></small>
-				</li> 
-				<li><small>This site is made with &#129505;, crafted by human, not by AI.</small></li></small>
 				<li>
-				<small>Text and visuals</small> 
+				<small>
+				<a href="#top">&uarr;</a>
+				</small>
+				</li> 
+
+				<li>
+				<small>
+					<small>This site is made with &#129505;, from scratch. Crafted</small> <a href="https://andremchang.github.io/by_homo_sapiens_not_by_ai"><small><small>BY <i>Homo sapiens</i>, NOT BY AI.</small></small></a>
+				</small>
+				</li>
+
+				<li>
 				<a href="https://andremchang.github.io/by_homo_sapiens_not_by_ai">
 				<img src="https://andremchang.github.io/assets/byhomosapiens.png" width="60%" height="auto" alt="“By Homo sapiens, not by AI badge”"/></a>
 				</li> 
-				<li><small>Copyright &#169; 2026 ANDRÉ M. CHANG. All rights reserved.<br>Prohibited for Generative AI/ML training.</small>
+
+				<li>
+				<small>
+					<small>Copyright &#169; 2026 ANDRÉ M. CHANG. All rights reserved.
+					<br>Prohibited for Generative AI/ML training.</small>
+				</small>
 				</li>
-				<li><small>Fingerprint: B0C3 C562 C765 ECE8 C110 7897 FDE0 F655 E348 A81E</small>
+				
+				<li>
+				<small>
+					<small>Fingerprint: B0C3 C562 C765 ECE8 C110 7897 FDE0 F655 E348 A81E</small>
+				</small>
 				</li>
-				<li><small><a href="https://keys.openpgp.org/search?q=B0C3+C562+C765+ECE8+C110+7897+FDE0+F655+E348+A81E" target="_blank"><small>[Public key]</small></a>
+
+				<li>
+				<small>
+					<a href="https://keys.openpgp.org/search?q=B0C3+C562+C765+ECE8+C110+7897+FDE0+F655+E348+A81E" target="_blank"><small><small>[Public key]</small></small></a>
+				</small>
 				</li>
-				<li><small>Updated on Aug 24, 2026.</small>
+
+				<li>
+				<small>
+					<small>Updated on Aug 25, 2026.</small>
+				</small>
 				</li>
 
 			</ul>
